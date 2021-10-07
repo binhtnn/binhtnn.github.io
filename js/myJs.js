ec0f153c8b8ee739dbd9a9f7d446d4e4dc535cc7
binhtnn <92083061+binhtnn@users.noreply.github.com>
2021-10-07 17:29:45 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
 
   // switch button position
   function switchButton() {
-    var audio = new Audio("sound/sound-duck.mp3");
+    var audio = new Audio("sound/sound_duck.mp3");
     audio.play();
     var leftNo = $("#no").css("left");
     var topNO = $("#no").css("top");
@@ -60,7 +60,7 @@ $(document).ready(function () {
   }
   // move random button position
   function moveButton() {
-    var audio = new Audio("sound/sound-Swish1.mp3");
+    var audio = new Audio("sound/sound_Swish1.mp3");
     audio.play();
     if (screen.width <= 600) {
       var x = Math.random() * 300;
@@ -107,7 +107,7 @@ $(document).ready(function () {
 
   // show popup
   $("#yes").click(function () {
-    var audio = new Audio("sound/sound-tick.mp3");
+    var audio = new Audio("sound/sound_tick.mp3");
     audio.play();
     Swal.fire({
       title: textConfig.text7,
